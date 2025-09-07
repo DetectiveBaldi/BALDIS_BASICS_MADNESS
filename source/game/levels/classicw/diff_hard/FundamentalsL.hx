626e7c35d7d0ded3cae46970d51cea1379998b4f
KayipKux0 <167240977+KayipKux0@users.noreply.github.com>
2025-09-07 22:04:02 +0300

    406

--- source/game/levels/classicw/diff_hard/FundamentalsL.hx
+++ source/game/levels/classicw/diff_hard/FundamentalsL.hx
@@ -34,5 +34,108 @@ class FundamentalsL extends PlayState
         fundamentalsS = cast (stage, FundamentalsS);
 
         super.create();
+
+        fundamentalsS.hall0.visible = true;
+
+        cameraLock = FOCUS_CAM_POINT;
+
+        cameraPoint.centerTo();
+
+        gameCameraZoom = 0.65;
+
+        opponent.setPosition(-650.0, -520.0);
+
+        opponent.color = 0xA09A85;
+
+        player.setPosition(720.0, 205.0);
+
+        player.color = 0xB8B19C;
+
+        gameCamera.alpha = 0.0;
+
+        AssetCache.getGraphic("game/Character/principal");
+
+        AssetCache.getGraphic("game/Character/bully");
+
+        AssetCache.getGraphic("game/Character/bf-face-left");
+    }
+
+    override function stepHit(step:Int):Void
+    {
+        super.stepHit(step);
+
+        if (step >= 10.0 && step <= 15.0)
+        {
+            if (step % 2 == 0.0)
+                gameCamera.alpha += 0.25;
+        }
+
+        if (step == 16.0)
+            gameCamera.alpha += 1.0;
+
+        if (step == 272.0)
+        {
+            if (Options.flashingLights)
+                gameCamera.flash(FlxColor.WHITE, conductor.beatLength * 2.0 * 0.001, null, true);
+
+            gameCameraZoom = 0.65;
+
+            cameraLock = FOCUS_CAM_CHAR;
+        }
+
+        if (step == 784.0)
+        {
+            if (Options.flashingLights)
+                gameCamera.flash(FlxColor.WHITE, conductor.beatLength * 2.0 * 0.001, null, true);
+
+            playField.scoreClip.visible = playField.scoreText.visible = playField.healthBar.visible = 
+            playField.timerClock.visible = playField.timerNeedle.visible = false;
+
+            cameraLock = FOCUS_CAM_POINT;
+
+            cameraPoint.centerTo();
+
+            var opp:Character = new Character(conductor, 0.0, 0.0, Character.getConfig("principal"));
+            opp.setPosition(0.0, 22.0);
+            opp.color = 0xADA493;
+            opponents.add(opp);
+
+            player.visible = false;
+        }
+
+        if (step == 848.0)
+        {
+            if (Options.flashingLights)
+                gameCamera.flash(FlxColor.WHITE, conductor.beatLength * 2.0 * 0.001, null, true);
+
+            fundamentalsS.hall0.visible = false;
+            fundamentalsS.office.visible = true;
+
+            playField.scoreClip.visible = playField.scoreText.visible = playField.healthBar.visible = 
+            playField.timerClock.visible = playField.timerNeedle.visible = true;
+
+            cameraLock = FOCUS_CAM_CHAR;
+
+            opponent.visible = false;
+            
+            var plr:Character = getPlayer("bf-face-left");
+            plr.setPosition(720.0, 205.0);
+            plr.visible = true;
+            player = plr;
+
+            var opp:Character = getOpponent("principal");
+            opp.setPosition(-60.0, 60.0);
+            opponent = opp;
+        }
+    }
+
+    override function beatHit(beat:Int):Void
+    {
+        super.beatHit(beat);
+
+        if (beat >= 64.0 && beat <= 67.0)
+        {
+            gameCameraZoom += 0.025;
+        }
     }
 }
--- source/game/stages/classicw/diff_hard/FundamentalsS.hx
+++ source/game/stages/classicw/diff_hard/FundamentalsS.hx
@@ -6,8 +6,24 @@ import flixel.util.FlxColor;
 
 class FundamentalsS extends Stage
 {
+    public var hall0:FlxSprite;
+
+    public var office:FlxSprite;
+
+    public var office2:FlxSprite;
+
+    public var hall1:FlxSprite;
+
     public function new():Void
     {
         super();
+
+        hall0 = getSprite("hall0", 2, 2);
+
+        office = getSprite("office", 2, 2);
+
+        office2 = getSprite("office2", 2, 2);
+
+        hall1 = getSprite("hall1", 2, 2);
     }
 }
